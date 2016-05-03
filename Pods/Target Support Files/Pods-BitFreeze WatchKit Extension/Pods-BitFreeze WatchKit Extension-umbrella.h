f6d674b34537df9f098fc456a66d863831c924b6
Thiago Alves <thiago10.alves@hotmail.com.br>
2016-05-03 14:23:13 -0300

    Clear Project

--- Pods/Target Support Files/Pods-BitFreeze WatchKit Extension/Pods-BitFreeze WatchKit Extension-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_BitFreeze_WatchKit_ExtensionVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BitFreeze_WatchKit_ExtensionVersionString[];
-
